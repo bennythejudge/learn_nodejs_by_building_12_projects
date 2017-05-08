5a23fb4f554019687ed33c9fb75febfcaa8e2e77
Benedetto Lo Giudice <logube@gmail.com>
2017-05-08 23:07:54 +0200

    Add an extra exclude

--- computer_repair/app.js
+++ computer_repair/app.js
@@ -15,8 +15,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 // routes
 app.get('/', function(req, res) {
   console.log('routing to /');
-  res.render('index');
+  res.render('index', { title: 'Computer not working' });
 });
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+
+app.post('/contact/send', function(req, res){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'logube@gmail.com',
+      pass: 'gkdlxhuftprziprz'
+    }
+  });
+  var mailOptions = {
+    from: 'Ben <logube@gmail.com',
+    to: 'benedettologiudice@gmail.com',
+    subject: 'Learning node',
+    text: 'learning nodejs by night Name ' + req.body.name +
+          ' Email: ' + req.body.email +
+          ' Message: ' + req.body.message,
+    html: '<p>'+req.body.message+'</p><ul><li>Email '+req.body.email+'</li></ul>'
+  }
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log('error sending email ' + error );
+      res.redirect('/');
+    } else {
+      console.log('message sent: ' + info.response);
+      res.redirect('/');
+    }
+  })
+});
+
 
 app.listen(3000);
 console.log("Server is running on port 3000");
